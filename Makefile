50abe018fcf24f992e6b6022ad095c172b96f338
Leandro Araújo <leandrocaaraujo@gmail.com>
2024-02-03 18:00:29 +0000

    Download site data to file

--- Makefile
+++ Makefile
@@ -2,6 +2,9 @@
 help: # Show help for each of the Makefile target.
 	@grep -E '^[a-zA-Z0-9 _]+:.*#'  Makefile | sort | while read -r l; do printf "\033[1;32m$$(echo $$l | cut -f 1 -d':')\033[00m:$$(echo $$l | cut -f 2- -d'#')\n"; done
 
+install_requirements: # Installl packages requirements for run main code
+	python -m pip install -r requirements.txt
+
 install_requirements_test: # Installl packages requirements for test and linter
 	python -m pip install -r requirements_test.txt
 
--- main.py
+++ main.py
@@ -1,4 +1,30 @@
+import requests
+import os
 
+
+PAGES = [
+    "https://en.wikipedia.org/wiki/UEFA_Champions_League",
+    "https://en.wikipedia.org/wiki/Copa_Am%C3%A9rica",
+    "https://en.wikipedia.org/wiki/UEFA_Nations_League",
+    "https://en.wikipedia.org/wiki/UEFA_European_Championship",
+    "https://en.wikipedia.org/wiki/FIFA_World_Cup"
+]
+
+OUTPUT_FOLDER = 'output_data'
 
 if __name__ == '__main__':
     print('Starting')
+
+    # create output_foled if not exists
+
+    if not os.path.exists(OUTPUT_FOLDER):
+        os.makedirs(OUTPUT_FOLDER)
+
+    for page in PAGES:
+        resp = requests.get(page)
+
+        print(resp.text)
+        filename = page.split('/')[-1]
+
+        with open(f"{OUTPUT_FOLDER}/{filename}.html", "w") as f:
+            f.write(resp.text)
