35a84684e2bcf640b5ced32d44502f05d90b6b2a
Leandro Araújo <leandrocaaraujo@gmail.com>
2024-02-04 22:24:19 +0000

    Generate Word2Vec from wikipedia content

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import requests
-import os
+import re
+
+from bs4 import BeautifulSoup
+import nltk
+from gensim.models import Word2Vec
 
 
 PAGES = [
@@ -10,21 +14,51 @@ PAGES = [
     "https://en.wikipedia.org/wiki/FIFA_World_Cup"
 ]
 
-OUTPUT_FOLDER = 'output_data'
+
+class ProcessContent():
+    page = ""
+    page_content = []
+    merge_text = ""
+    sentences = []
+    model = Word2Vec()
+
+    def __init__(self, page="", page_content="") -> None:
+        self.page = page
+        self.page_content = page_content
+
+    def get_text_data(self):
+        """Merge page_content in one string"""
+
+        for content in self.page_content:
+            self.merge_text += content.text
+
+        self.merge_text = re.sub(r'\[[0-9]*\]', ' ', self.merge_text)
+        self.merge_text = re.sub(r'\s+', ' ', self.merge_text)
+        self.merge_text = self.merge_text.lower()
+        self.merge_text = re.sub(r'[@#\$%&\*\(\)\<\>\'\":;\]\[-]', ' ', self.merge_text)
+        self.merge_text = re.sub(r'\d', ' ', self.merge_text)
+        self.merge_text = re.sub(r'\s+', ' ', self.merge_text)
+
+    def tokenize_text(self):
+        """Tokenize text"""
+        self.sentences = nltk.sent_tokenize(self.merge_text)
+
+        self.sentences = [nltk.word_tokenize(sentence) for sentence in self.sentences]
+
+    def run(self):
+        print(f'Running process to page {self.page}')
+        self.get_text_data()
+        self.tokenize_text()
+        self.model = Word2Vec(self.sentences, min_count=1)
+
 
 if __name__ == '__main__':
-    print('Starting')
-
-    # create output_foled if not exists
-
-    if not os.path.exists(OUTPUT_FOLDER):
-        os.makedirs(OUTPUT_FOLDER)
+    nltk.download('punkt')
 
     for page in PAGES:
         resp = requests.get(page)
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        content = soup.find_all('p')
 
-        print(resp.text)
-        filename = page.split('/')[-1]
-
-        with open(f"{OUTPUT_FOLDER}/{filename}.html", "w") as f:
-            f.write(resp.text)
+        process = ProcessContent(page=page, page_content=content)
+        process.run()
